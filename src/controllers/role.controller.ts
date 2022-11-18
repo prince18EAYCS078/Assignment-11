b6aa624ea54a5d70d60b45665d3bfc57a1b5648c
prince suman <prince.suman@sourcefuse.com>
2022-11-18 21:41:59 +0530

    first commit

---
+++ src/controllers/role.controller.ts
@@ -0,0 +1,162 @@
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where
+} from '@loopback/repository';
+import {
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
+} from '@loopback/rest';
+import {authenticate, STRATEGY} from 'loopback4-authentication';
+import {authorize} from 'loopback4-authorization';
+import {Role} from '../models';
+import {RoleRepository} from '../repositories';
+
+export class RoleController {
+  constructor(
+    @repository(RoleRepository)
+    public roleRepository: RoleRepository,
+  ) { }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['CreateRoles']})
+  @post('/roles')
+  @response(200, {
+    description: 'Role model instance',
+    content: {'application/json': {schema: getModelSchemaRef(Role)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Role, {
+            title: 'NewRole',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    role: Omit<Role, 'id'>,
+  ): Promise<Role> {
+    return this.roleRepository.create(role);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['GetRolesCount']})
+  @get('/roles/count')
+  @response(200, {
+    description: 'Role model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(
+    @param.where(Role) where?: Where<Role>,
+  ): Promise<Count> {
+    return this.roleRepository.count(where);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['GetRoles']})
+  @get('/roles')
+  @response(200, {
+    description: 'Array of Role model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Role, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Role) filter?: Filter<Role>,
+  ): Promise<Role[]> {
+    return this.roleRepository.find(filter);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['UpdateRoles']})
+  @patch('/roles')
+  @response(200, {
+    description: 'Role PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Role, {partial: true}),
+        },
+      },
+    })
+    role: Role,
+    @param.where(Role) where?: Where<Role>,
+  ): Promise<Count> {
+    return this.roleRepository.updateAll(role, where);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['GetRoleById']})
+  @get('/roles/{id}')
+  @response(200, {
+    description: 'Role model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Role, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(Role, {exclude: 'where'}) filter?: FilterExcludingWhere<Role>
+  ): Promise<Role> {
+    return this.roleRepository.findById(id, filter);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['UpdateRoleById']})
+  @patch('/roles/{id}')
+  @response(204, {
+    description: 'Role PATCH success',
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Role, {partial: true}),
+        },
+      },
+    })
+    role: Role,
+  ): Promise<void> {
+    await this.roleRepository.updateById(id, role);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['UpdateRoleById']})
+  @put('/roles/{id}')
+  @response(204, {
+    description: 'Role PUT success',
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() role: Role,
+  ): Promise<void> {
+    await this.roleRepository.replaceById(id, role);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['DeleteRoleById']})
+  @del('/roles/{id}')
+  @response(204, {
+    description: 'Role DELETE success',
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.roleRepository.deleteById(id);
+  }
+}
---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,190 @@
+import {inject, service} from '@loopback/core';
+import {
+  Count,
+  CountSchema,
+  Filter,
+  FilterExcludingWhere,
+  repository,
+  Where
+} from '@loopback/repository';
+import {
+  del, get,
+  getModelSchemaRef, param, patch, post, put, requestBody,
+  response
+} from '@loopback/rest';
+import {securityId, UserProfile} from '@loopback/security';
+import {authenticate, AuthenticationBindings, STRATEGY} from 'loopback4-authentication';
+import {authorize} from 'loopback4-authorization';
+import {User} from '../models';
+import {UserRepository} from '../repositories';
+import {jwtService} from '../services/jwt.service';
+
+export class UserController {
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+    @service(jwtService) public jwtService: jwtService,
+  ) { }
+
+  @authenticate(STRATEGY.LOCAL)
+  @authorize({permissions: ['*']})
+  @post('/users/login')
+  @response(200, {
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+        },
+      },
+    },
+  })
+  async login(
+    @requestBody()
+    credentials: {
+      username: 'string';
+      password: 'string';
+    },
+    @inject(AuthenticationBindings.CURRENT_USER) currentUser: User,
+  ): Promise<{token: string}> {
+    let user: UserProfile = {
+      [securityId]: currentUser.id!.toString(),
+      id: currentUser.id,
+      userName: currentUser.username,
+      firstName: currentUser.firstName,
+    };
+
+    let token = await this.jwtService.generateToken(user);
+    return {token};
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @authorize({permissions: ['*']})
+  @post('/users')
+  @response(200, {
+    description: 'User model instance',
+    content: {'application/json': {schema: getModelSchemaRef(User)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {
+            title: 'NewUser',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    user: Omit<User, 'id'>,
+  ): Promise<User> {
+    return this.userRepository.create(user);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+
+  @get('/users/count')
+  @response(200, {
+    description: 'User model count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async count(
+    @param.where(User) where?: Where<User>,
+  ): Promise<Count> {
+    return this.userRepository.count(where);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @get('/users')
+  @response(200, {
+    description: 'Array of User model instances',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(User, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(User) filter?: Filter<User>,
+  ): Promise<User[]> {
+    return this.userRepository.find(filter);
+  }
+  @authenticate(STRATEGY.BEARER)
+  @patch('/users')
+  @response(200, {
+    description: 'User PATCH success count',
+    content: {'application/json': {schema: CountSchema}},
+  })
+  async updateAll(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+    @param.where(User) where?: Where<User>,
+  ): Promise<Count> {
+    return this.userRepository.updateAll(user, where);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @get('/users/{id}')
+  @response(200, {
+    description: 'User model instance',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(User, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.number('id') id: number,
+    @param.filter(User, {exclude: 'where'}) filter?: FilterExcludingWhere<User>
+  ): Promise<User> {
+    return this.userRepository.findById(id, filter);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @patch('/users/{id}')
+  @response(204, {
+    description: 'User PATCH success',
+  })
+  async updateById(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(User, {partial: true}),
+        },
+      },
+    })
+    user: User,
+  ): Promise<void> {
+    await this.userRepository.updateById(id, user);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @put('/users/{id}')
+  @response(204, {
+    description: 'User PUT success',
+  })
+  async replaceById(
+    @param.path.number('id') id: number,
+    @requestBody() user: User,
+  ): Promise<void> {
+    await this.userRepository.replaceById(id, user);
+  }
+
+  @authenticate(STRATEGY.BEARER)
+  @del('/users/{id}')
+  @response(204, {
+    description: 'User DELETE success',
+  })
+  async deleteById(@param.path.number('id') id: number): Promise<void> {
+    await this.userRepository.deleteById(id);
+  }
+}
